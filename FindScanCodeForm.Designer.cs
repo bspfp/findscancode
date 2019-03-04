deef1b1dfee94a9e3bae0f52f9979ca01dae2e04
BS <kwanghochoi@hotmail.com>
2019-03-04 13:19:27 +0900

    첫 업로드

---
+++ FindScanCodeForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace FindScanCode
+{
+    partial class FindScanCodeForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            this.lbScanCode = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbVKey = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(13, 13);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(125, 24);
+            label1.TabIndex = 0;
+            label1.Text = "ScanCode:";
+            // 
+            // lbScanCode
+            // 
+            this.lbScanCode.AutoSize = true;
+            this.lbScanCode.Location = new System.Drawing.Point(145, 13);
+            this.lbScanCode.Name = "lbScanCode";
+            this.lbScanCode.Size = new System.Drawing.Size(114, 24);
+            this.lbScanCode.TabIndex = 1;
+            this.lbScanCode.Text = "00000000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(266, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "VKey:";
+            // 
+            // lbVKey
+            // 
+            this.lbVKey.AutoSize = true;
+            this.lbVKey.Location = new System.Drawing.Point(344, 13);
+            this.lbVKey.Name = "lbVKey";
+            this.lbVKey.Size = new System.Drawing.Size(270, 24);
+            this.lbVKey.TabIndex = 3;
+            this.lbVKey.Text = "00000000000000000000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 53);
+            this.Controls.Add(this.lbVKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbScanCode);
+            this.Controls.Add(label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FindScanCode";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbScanCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbVKey;
+    }
+}
+
---
+++ FindScanCodeForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace FindScanCode {
+    public partial class FindScanCodeForm : Form {
+        public FindScanCodeForm() {
+            InitializeComponent();
+            if (!RegisterRawInput())
+                throw new ApplicationException("Raw input 등록 실패");
+        }
+
+        protected override void WndProc(ref Message message) {
+            ProcRawInput(ref message);
+            base.WndProc(ref message);
+        }
+
+        // Raw Input 등록
+        private bool RegisterRawInput() {
+            RAWINPUTDEVICE[] rid = new RAWINPUTDEVICE[1];
+
+            // 일반 키보드
+            rid[0].usUsagePage = HIDUsagePage.Generic;
+            rid[0].usUsage = HIDUsage.Keyboard;
+
+            // 창이 백그라운드에 있어도 입력을 받으려면 아래 dwFlags 수정하기
+            // rid[0].dwFlags = RawInputDeviceFlags.InputSink;
+            rid[0].dwFlags = RawInputDeviceFlags.None;
+
+            rid[0].hwndTarget = Handle;
+
+            return RegisterRawInputDevices(rid, 1, Marshal.SizeOf(rid[0]));
+        }
+
+        // 메시지 처리
+        public void ProcRawInput(ref Message message) {
+
+            // 입력 메시지 일 때에만
+            if (message.Msg != WM_INPUT)
+                return;
+
+            RAWINPUT input = new RAWINPUT();
+            int size = Marshal.SizeOf(typeof(RAWINPUT));
+            int outSize = size;
+            if (GetRawInputData(message.LParam, RawInputCommand.Input, out input, ref outSize, Marshal.SizeOf(typeof(RAWINPUTHEADER))) != size)
+                return;
+
+            // 키보드 다운만 처리
+            if (input.header.dwType != RawInputType.Keyboard)
+                return;
+            if (input.keyboard.Message != WM_KEYDOWN && input.keyboard.Message != WM_SYSKEYDOWN)
+                return;
+
+            // Keys.OemClear == 254 까지만 유효한 키입력으로 간주
+            if (input.keyboard.VKey > (ushort)Keys.OemClear)
+                return;
+
+            lbVKey.Text = Enum.GetName(typeof(Keys), input.keyboard.VKey);
+
+            if (input.keyboard.Flags == 2)
+                lbScanCode.Text = string.Format("E0_{0:X2}", input.keyboard.MakeCode);
+            else if (input.keyboard.Flags == 4)
+                lbScanCode.Text = string.Format("E1_{0:X2}", input.keyboard.MakeCode);
+            else
+                lbScanCode.Text = string.Format("00_{0:X2}", input.keyboard.MakeCode);
+        }
+
+        // 아래는 Win32 API 호출을 위한 상수, 구조체, API 들
+
+        private const uint WM_INPUT = 0x00FF;
+        private const uint WM_KEYDOWN = 0x0100;
+        private const uint WM_SYSKEYDOWN = 0x0104;
+
+        public enum HIDUsagePage : ushort { Generic = 0x01 }
+        public enum HIDUsage : ushort { Keyboard = 0x06 }
+        [Flags()]
+        public enum RawInputDeviceFlags {
+            None = 0,
+            InputSink = 0x00000100,
+            NoHotKeys = 0x00000200,
+            AppKeys = 0x00000400
+        }
+        public enum RawInputType {
+            Mouse = 0,
+            Keyboard = 1,
+            HID = 2,
+            Other = 3
+        }
+        public enum RawInputCommand {
+            Input = 0x10000003,
+            Header = 0x10000005
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWINPUTDEVICE {
+            public HIDUsagePage usUsagePage;
+            public HIDUsage usUsage;
+            public RawInputDeviceFlags dwFlags;
+            public IntPtr hwndTarget;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWINPUTHEADER {
+            public RawInputType dwType;
+            public int dwSize;
+            public IntPtr hDevice;
+            public IntPtr wParam;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWKEYBOARD {
+            public ushort MakeCode;
+            public ushort Flags;
+            public ushort Reserved;
+            public ushort VKey;
+            public uint Message;
+            public uint ExtraInformation;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RAWINPUT {
+            public RAWINPUTHEADER header;
+            public RAWKEYBOARD keyboard;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterRawInputDevices([MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] RAWINPUTDEVICE[] pRawInputDevices, int uiNumDevices, int cbSize);
+
+        [DllImport("user32.dll")]
+        public static extern int GetRawInputData(IntPtr hRawInput, RawInputCommand uiCommand, out RAWINPUT pData, ref int pcbSize, int cbSizeHeader);
+    }
+}
